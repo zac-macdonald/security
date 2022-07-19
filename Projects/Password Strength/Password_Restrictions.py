51ac75510e4b320515ba425892f7139dcabb3c7d
zac-macdonald <109450311+zac-macdonald@users.noreply.github.com>
2022-07-19 17:53:56 +1000

    fixed typos, and finished password functions

--- Projects/Password Strength/Password_Restrictions.py
+++ Projects/Password Strength/Password_Restrictions.py
@@ -14,18 +14,16 @@ Here's an example senario:
     Generally, because of the complexity of Mary's password, the attacker would be able to guess the password in a very short amount of time.
 
     Enter the bank. The bank updates their password policies. Mary now needs a password that is at least
-    8 characters in length, and contains three numbers. She can't use the password "carrot11" anymore,
-    so she uses "p@s$w04d" instead.
+    8 characters in length, and contains three numbers. She can't use the password "password" anymore,
+    so she uses "password111" instead. The problem with this is that inherently, the password is still quite easy to guess.
+    Humans by nature take the path of least resistance, hence the subtle change in the password.
 
-    The problem here, is that even though she was forced to use a password that adheres to the bank's password policies,
-    the attacker would still be able to guess the password using a brute-force attack because the password is still easy to guess.
-    The password policy has also now made it easier, by narrowing the range of characters that can be used in cracking the password.
-    You might be wondering to yourself, "But what if I just make a password that is harder to guess that adheres to the bank's password policies?"
-    The majority of people would change their original password to meet the requirements of the new password policies, and in that case,
-    It would increase password entropy, however, by assigning these rules, the password time to crack has been lowered, by removing
-    the need to check for passwords without numbers, and that are under 8 characters long.
-    Since password cracking is exponential, it saves a lot of time and effort by not having to check for passwords that are
-    outside of the range of the password policies.
+    So, an attacker now wants to hack the bank again, and this time, the attacker sees the password policy.
+    Now, instead of the attacker trying to brute force a password with all combinations of characters, 
+    less than the length of 8, the attacker can try to brute force the password with all combinations of characters,
+    more than the length of 8, and also contains three numbers.
+    To put it simply, the attacker has just narrowed the amount of passwords that they have to check, 
+    making it easier for the attacker to crack the password.
 
     To calculate the number of total unique passwords that can be created, we can use the following formula:
 
@@ -35,35 +33,32 @@ Here's an example senario:
 
     The formula above calculates the total number of unique passwords that can be created, given the number of possible characters,
     and the length of the password. 8 RTX-3090's can crack at 471.7 GH/s. (see https://www.onlinehashcrack.com/tools-benchmark-hashcat-nvidia-rtx-3090.php) 
-    Which means, that if the password length is 8, and the number of possible characters is 24,
-    The total combination of passwords for the given password length is 128063081718016.
-
-
+    Which means, that if the password length is 8, the number of possible characters is 94,
+    and total combination of passwords for the given password length is 6095689385410816,
+    the time it would take to crack the password, is around 1 hour and 45 minutes. (Calculated below)
 """
-import numpy
+
+import datetime
 
 def possible_characters(password):
-    """
-    Calculates the total number of possible characters that can be used in a password.
-    """
-    # Get the total number of possible characters
     L = len(password)
-    # Get the total number of possible symbols (alphabet lower, and upper, and numbers)
     S = 94
-    # Calculate the total number of possible characters
-    possible_characters = S**L
-    return possible_characters
+    return S**L
 
-pwd = []
-for i in range(1, 13):
-    pwd.append(chr(i))
-    e = possible_characters(pwd)
-    time = e/471700000000/2
-    import datetime
+def password_time(password, max):
+    pwd = []
+    for i in range(1, max):
+        pwd.append(chr(i))
+        e = possible_characters(pwd)
+        # 471.7 GH/s > 47170000000
+        # we devide by 2 because in a brute force attack,
+        # if the attacker gets to 50% of the total possible passwords,
+        # and it has not yet cracked the password, they will have an
+        # increasing chance of cracking the password after each guess.
+        time = e/471700000000/2
+        res = datetime.timedelta(seconds =time)
+        print(f"{i}: {e}   :::   {res}")
 
-    res = datetime.timedelta(seconds =time)
-
-    print(f"{i}: {e}   :::   {res}")
-
-d = 24986644000165537792/471700000000
-print(datetime.timedelta(seconds =d))
+if __name__ == '__main__':
+    pwd = input("Enter a password: ")
+    password_time(input, 94)
